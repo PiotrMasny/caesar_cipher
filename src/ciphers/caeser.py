f2371a094ad8cd9803852b1745a8265afc1bb846
PiotrMasny <piotr.masny2000@gmail.com>
2024-06-08 15:17:27 +0200

    Added polish signs handling

--- src/ciphers/caeser.py
+++ src/ciphers/caeser.py
@@ -3,23 +3,29 @@ class CaesarCipher:
         self.alphabet = alphabet
 
     def encrypt(self, text_to_encrypt: str, key: int) -> str:
-        encrypted_text = ''
+        encrypted_text = ""
         for sign in text_to_encrypt.lower():
-            if not sign.isalpha():
-                encrypted_text += sign
-            else:
-                encrypted_text += self.alphabet[
-                    (self.alphabet.index(sign) + key) % len(self.alphabet)
+            try:
+                if not sign.isalpha():
+                    encrypted_text += sign
+                else:
+                    encrypted_text += self.alphabet[
+                        (self.alphabet.index(sign) + key) % len(self.alphabet)
                     ]
+            except ValueError:
+                encrypted_text += sign
         return encrypted_text
 
     def decrypt(self, text_to_decrypt: str, key: int) -> str:
-        decrypted_text = ''
+        decrypted_text = ""
         for sign in text_to_decrypt.lower():
-            if not sign.isalpha():
-                decrypted_text += sign
-            else:
-                decrypted_text += self.alphabet[
-                    (self.alphabet.index(sign) - key) % len(self.alphabet)
+            try:
+                if not sign.isalpha():
+                    decrypted_text += sign
+                else:
+                    decrypted_text += self.alphabet[
+                        (self.alphabet.index(sign) - key) % len(self.alphabet)
                     ]
+            except ValueError:
+                decrypted_text += sign
         return decrypted_text
--- tests/test_caeser_cipher.py
+++ tests/test_caeser_cipher.py
@@ -1,53 +1,117 @@
+from string import ascii_lowercase
+
 import pytest
 
 from src.ciphers.caeser import CaesarCipher
 
 
-@pytest.mark.parametrize("text_to_encrypt, key, encrypted_text", [
-    ('opportunity', 2, 'qrrqtvwpkva'),
-    ('5 diamonds', 5, '5 infrtsix')
-])
-def test_encrypt_lowercase_sentence_with_positive_key(text_to_encrypt, key, encrypted_text):
-    cipher = CaesarCipher([chr(letter) for letter in range(ord('a'), ord('z') + 1)])  # from string import ascii_lowercase
+@pytest.mark.parametrize(
+    "text_to_encrypt, key, encrypted_text",
+    [("opportunity", 2, "qrrqtvwpkva"), ("5 diamonds", 5, "5 infrtsix")],
+)
+def test_encrypt_lowercase_sentence_with_positive_key(
+    text_to_encrypt, key, encrypted_text
+):
+    cipher = CaesarCipher(list(ascii_lowercase))
     actual_result = cipher.encrypt(text_to_encrypt, key)
     expected_result = encrypted_text
     assert actual_result == expected_result
 
 
-@pytest.mark.parametrize("text_to_decrypt, key, decrypted_text", [
-    ('yjkuv', 2, 'whist'),
-    ('fqq ns', 5, 'all in')
-])
-def test_decrypt_lowercase_sentence_with_positive_key(text_to_decrypt, key, decrypted_text):
-    cipher = CaesarCipher([chr(letter) for letter in range(ord('a'), ord('z') + 1)])
+@pytest.mark.parametrize(
+    "text_to_decrypt, key, decrypted_text",
+    [("yjkuv", 2, "whist"), ("fqq ns", 5, "all in")],
+)
+def test_decrypt_lowercase_sentence_with_positive_key(
+    text_to_decrypt, key, decrypted_text
+):
+    cipher = CaesarCipher(list(ascii_lowercase))
     actual_result = cipher.decrypt(text_to_decrypt, key)
     expected_result = decrypted_text
     assert actual_result == expected_result
 
 
-@pytest.mark.parametrize("text_to_encrypt, key, encrypted_text", [
-    ('Bluff', 2, 'dnwhh'),
-    ('After tHe GAME!', 5, 'fkyjw ymj lfrj!')
-])
-def test_encrypt_uppercase_sentence_with_positive_key(text_to_encrypt, key, encrypted_text):
-    cipher = CaesarCipher([chr(letter) for letter in range(ord('a'), ord('z') + 1)])
+@pytest.mark.parametrize(
+    "text_to_encrypt, key, encrypted_text",
+    [("Bluff", 2, "dnwhh"), ("After tHe GAME!", 5, "fkyjw ymj lfrj!")],
+)
+def test_encrypt_uppercase_sentence_with_positive_key(
+    text_to_encrypt, key, encrypted_text
+):
+    cipher = CaesarCipher(list(ascii_lowercase))
     actual_result = cipher.encrypt(text_to_encrypt, key)
     expected_result = encrypted_text
     assert actual_result == expected_result
 
 
-@pytest.mark.parametrize("text_to_decrypt, key, decrypted_text", [
-    ('ejGEmocVg', 2, 'checkmate'),
-    ('xytsjbfQQ Fyyfhp', 5, 'stonewall attack')
-])
-def test_decrypt_uppercase_sentence_with_positive_key(text_to_decrypt, key, decrypted_text):
-    cipher = CaesarCipher([chr(letter) for letter in range(ord('a'), ord('z') + 1)])
+@pytest.mark.parametrize(
+    "text_to_decrypt, key, decrypted_text",
+    [("ejGEmocVg", 2, "checkmate"), ("xytsjbfQQ Fyyfhp", 5, "stonewall attack")],
+)
+def test_decrypt_uppercase_sentence_with_positive_key(
+    text_to_decrypt, key, decrypted_text
+):
+    cipher = CaesarCipher(list(ascii_lowercase))
     actual_result = cipher.decrypt(text_to_decrypt, key)
     expected_result = decrypted_text
     assert actual_result == expected_result
+
 
 # TODO
 # testy dal znaków specjalnych ze sa ignorowane ./*/- oraz ąćłż
+
+
+@pytest.mark.parametrize(
+    "text_to_encrypt, key, encrypted_text",
+    [("be-right-back", 2, "dg-tkijv-dcem"), ("i'm.on.it", 5, "n'r.ts.ny")],
+)
+def test_encrypt_special_signs_sentence_with_positive_key(
+    text_to_encrypt, key, encrypted_text
+):
+    cipher = CaesarCipher(list(ascii_lowercase))
+    actual_result = cipher.encrypt(text_to_encrypt, key)
+    expected_result = encrypted_text
+    assert actual_result == expected_result
+
+
+@pytest.mark.parametrize(
+    "text_to_decrypt, key, decrypted_text",
+    [("ugg,aqw^uqqp", 2, "see,you^soon"), ('bj_bnqq"xjj', 5, 'we_will"see')],
+)
+def test_decrypt_special_signs_sentence_with_positive_key(
+    text_to_decrypt, key, decrypted_text
+):
+    cipher = CaesarCipher(list(ascii_lowercase))
+    actual_result = cipher.decrypt(text_to_decrypt, key)
+    expected_result = decrypted_text
+    assert actual_result == expected_result
+
+
+@pytest.mark.parametrize(
+    "text_to_encrypt, key, encrypted_text", [("dąb", 2, "fąd"), ("kęs", 5, "pęx")]
+)
+def test_encrypt_polish_signs_sentence_with_positive_key(
+    text_to_encrypt, key, encrypted_text
+):
+    cipher = CaesarCipher(list(ascii_lowercase))
+    actual_result = cipher.encrypt(text_to_encrypt, key)
+    expected_result = encrypted_text
+    assert actual_result == expected_result
+
+
+@pytest.mark.parametrize(
+    "text_to_decrypt, key, decrypted_text",
+    [("dtbfęm", 2, "brzdęk"), ("ptżzhm", 5, "kożuch")],
+)
+def test_decrypt_polish_signs_sentence_with_positive_key(
+    text_to_decrypt, key, decrypted_text
+):
+    cipher = CaesarCipher(list(ascii_lowercase))
+    actual_result = cipher.decrypt(text_to_decrypt, key)
+    expected_result = decrypted_text
+    assert actual_result == expected_result
+
+
 # dodac test dla pojedynczej litery z -> a oraz a -> z
 # dodac test dla psutego słowa
 # dodac test dla key=0
